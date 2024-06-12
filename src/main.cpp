caadfc43c807654c722176e50786dc9ad4e0e253
sivar2311 <sivar2311@gmail.com>
2024-06-12 18:28:46 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,44 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
 #include <Update.h>
-#include <WebServer.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
 
 #include "pages.h"
 
 const char* ssid     = "MYESP32";
 const char* password = "12345678";
 
-WebServer server(80);
+AsyncWebServer server(80);
 
-void handleRoot_GET() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
+void handleUpdate_GET(AsyncWebServerRequest* request) {
+    request->send(200, "text/html", update_GET);
 }
 
-void handleServerIndex_GET() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
-}
-
-void handleUpdate_POST() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+void handleUpdate_POST(AsyncWebServerRequest* request) {
+    request->send(200, "text/html", update_POST);
+    delay(1000);
     ESP.restart();
 }
 
-void handleUpdate_File_POST() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-        Serial.printf("Update: %s\n", upload.filename.c_str());
+void handleFirmwareUpdate(AsyncWebServerRequest* request, const String& filename, size_t index, uint8_t* data, size_t len, bool final) {
+    static size_t total;
+    total += len;
+    if (!index) {
+        Serial.printf("Update: %s", filename.c_str());
         if (!Update.begin(UPDATE_SIZE_UNKNOWN)) {  // start with max available size
             Update.printError(Serial);
         }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-        /* flashing firmware to ESP*/
-        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-            Update.printError(Serial);
-        }
-    } else if (upload.status == UPLOAD_FILE_END) {
+    }
+    /* flashing firmware to ESP*/
+    if (Update.write(data, len) != len) {
+        Update.printError(Serial);
+    } else {
+        Serial.print(".");
+    }
+    if (final) {
         if (Update.end(true)) {  // true to set the size to the current progress
-            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+            Serial.printf("\r\nUpdate Success: %u bytes received\nRebooting...\n", total);
+            total = 0;
         } else {
             Update.printError(Serial);
         }
@@ -52,24 +49,20 @@ void setupHotspot() {
     WiFi.softAP(ssid, password);
     Serial.println("Hotspot created:");
     Serial.printf("  SSID: \"%s\"\r\n  Password: \"%s\"\r\n", ssid, password);
-    Serial.printf("Connect to the network above, open a browser and navigate to http://%s\r\n", WiFi.softAPIP().toString().c_str());
-    Serial.println("To login use \"admin\"/\"admin\"");
+    Serial.printf("Connect to the network above, open a browser and navigate to http://%s/update\r\n", WiFi.softAPIP().toString().c_str());
 }
 
 void setupWebServer() {
-    server.on("/", HTTP_GET, handleRoot_GET);
-    server.on("/serverIndex", handleServerIndex_GET);
-    server.on("/update", HTTP_POST, handleUpdate_POST, handleUpdate_File_POST);
+    server.on("/update", HTTP_GET, handleUpdate_GET);
+    server.on("/update", HTTP_POST, handleUpdate_POST, handleFirmwareUpdate);
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
     server.begin();
 }
 
-void setup(void) {
+void setup() {
     Serial.begin(115200);
     setupHotspot();
     setupWebServer();
 }
 
-void loop(void) {
-    server.handleClient();
-    delay(1);
-}
+void loop() {}
--- src/pages.h
+++ src/pages.h
@@ -1,4 +1,4 @@
 #pragma once
 
-extern const char* serverIndex;
-extern const char* loginIndex;
+extern const char* update_GET;
+extern const char* update_POST;
